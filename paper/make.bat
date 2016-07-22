e1dd2bc07f79aef58eece9005ce8a6a3e8ad302f
Klemens Schindler <klemensschindler@gmail.com>
2016-07-22 14:53:58 +0200

    Add make batch file for paper

---
+++ paper/make.bat
@@ -0,0 +1,4 @@
+pdflatex --output-directory=build/ LWCSinglePaper.tex
+cd build/; bibtex LWCSinglePaper.aux; cd ..
+pdflatex --output-directory=build/ LWCSinglePaper.tex
+pdflatex --output-directory=build/ LWCSinglePaper.tex	
