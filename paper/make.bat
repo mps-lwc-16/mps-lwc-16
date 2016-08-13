ec5baedad4989e85b8bd4dd29f2e3557fa0268e9
Klemens Schindler <klemensschindler@gmail.com>
2016-08-13 22:49:38 +0200

    Fix make batch file for windows

--- paper/make.bat
+++ paper/make.bat
@@ -1,4 +1,6 @@
 pdflatex --output-directory=build/ LWCSinglePaper.tex
-cd build/; bibtex LWCSinglePaper.aux; cd ..
+cd build
+bibtex LWCSinglePaper.aux
+cd ..
 pdflatex --output-directory=build/ LWCSinglePaper.tex
 pdflatex --output-directory=build/ LWCSinglePaper.tex	
